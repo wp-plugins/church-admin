cf4f81fa6f68ea56713a02f8bef2fb668050d397
andy_moyle <andy_moyle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-08 16:12:08 +0000

    Minor tweaks
    git-svn-id: https://plugins.svn.wordpress.org/church-admin/trunk@569012 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -12,10 +12,10 @@ add_action('wp_head', 'church_admin_public_header');
 function church_admin_public_header()
 {
     global $church_admin_version;
-     wp_enqueue_style('Church Admin',CHURCH_ADMIN_INCLUDE_URL.'public.css');
+    wp_enqueue_style('Church Admin',CHURCH_ADMIN_INCLUDE_URL.'public.css');
     echo'<!-- church_admin v'.$church_admin_version.'-->
     <style>table.church_admin_calendar{width:';
     if(get_option('church_admin_calendar_width')){echo get_option('church_admin_calendar_width');}else {echo'700';}
-    
+    echo'</style>';
 }
 ?>
